7b92acb544c64457556d8db67ec8db501fa8439e
rju <rju@purdue.edu>
2016-11-20 06:19:10 -0600

    Finished Shopping Cart Cost Calculator

--- costCalculator.py
+++ costCalculator.py
@@ -3,7 +3,9 @@
 cart = []
 
 modeOptions = "Please select your mode:\n1. Quick Cost Calculator\n2. Shopping Cart\n"
-options = "Please select an option:\n1. Add Item\n2. Set Tax Rate\n3. Calculate Total Cost\n4. Exit"
+options = "Please select an option:\n1. Add Item\n2. Set Tax Rate\n3. Calculate Total Cost\n0. Exit\n"
+
+taxRate = 0
 
 class Item: 
 	def __init__(self, q, n, c):
@@ -12,19 +14,33 @@ class Item:
 		self.cost = c 
 
 def addItem():
-	numItems = input("Enter the number of Items you want to order: ")
-	cart.append(Item(numItems, "Wings", 0.65))
+	itemName = raw_input("Enter the name of the item: ")
+	numItems = input("Enter the number of " + itemName + " you want to purchase: ")
+	salesPrice = input("Enter sales price for each individual item: ")
+	cart.append(Item(numItems, itemName, salesPrice))
+	print "\n"
 
 def calcTotalCosts():
 	tcost = 0
-	for i in range(cart.length):
-		tcost += (cart[i].quantity * cart[i].cost)
+	for i in cart:
+		print "---------------------"
+		print str(i.quantity) + " x " + i.name + " @ " + str(i.cost) + " = " + str(round(i.quantity * i.cost, 2)) 
+		tcost += (i.quantity * i.cost)
+	
+	print "---------------------\n"
+	
+	if (taxRate != 0):
+		taxCost = tcost * ((taxRate/100.0))
+		print "Tax: $" + str(round(taxCost, 2))
+		tcost += taxCost
+		
+	print "Total Cost: $" + str(round(tcost, 2)) + "\n"
 
 mode = input(modeOptions)
 if (mode == 1):
 	numItems = input("Enter quantity: ")
 	salesPrice = input("Enter sales price of each item: ")
-	taxRate = input("Enter tax rate, or 0 if you not applicable")
+	taxRate = input("Enter tax rate, or 0 if not applicable: ")
 	
 	if (taxRate == 0):
 		cost = numItems * salesPrice
@@ -34,25 +50,24 @@ if (mode == 1):
 	print "Total Cost: $" + str(round(cost, 2))	
 
 elif (mode == 2):
-	print "This is mode 2"
+	selection = input(options)
+	while (selection != 0):
+		if (selection == 1):
+			addItem()
+		elif (selection == 2):
+			taxRate = input("Enter the tax rate in your area: ")
+		elif (selection == 3):
+			calcTotalCosts();	
+		else:	
+			print "Please enter a valid input: "
+		selection = input(options)
+		
+	# print "Tax = " + str(tax) + "%"
+# 	print "Number of Wings ordered: " + str(cart[0].quantity)
+# 	cost = cart[0].quantity * cart[0].cost * ((tax / 100.0) + 1)
+# 	
+# 	print "Total cost: $" + str(round(cost, 2))
 
 else:
 	print "Please select a valid mode."
 	mode = input(modeOptions)
-
-# selection = input(options)
-# while (selection != 4):
-# 	if (selection == 1):
-# 		addItem()
-# 	elif (selection == 2):
-# 		tax = input("Enter the tax rate in your area: ")
-# 	elif (selection == 3):
-# 		calcTotalCosts();	
-# 				
-# 	selection = input(options)
-# 		
-# print "Tax = " + str(tax) + "%"
-# print "Number of Wings ordered: " + str(cart[0].quantity)
-# cost = cart[0].quantity * cart[0].cost * ((tax / 100.0) + 1)
-# 
-# print "Total cost: $" + str(round(cost, 2))
