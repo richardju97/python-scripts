cbbdbb1f99e60820f7f9e9a035e340ff7bfc3a64
rju <rju@purdue.edu>
2017-02-02 02:57:15 -0500

    Added binary.py w/ descriptions of to be implemented functions

---
+++ binary.py
@@ -0,0 +1,30 @@
+# binary.py
+# Collection of various calculations/operations related to binary strings
+# Also includes some similar calculations that aren't exclusive to binary (e.g. decimal to hex)
+
+# Given Decimal, Convert to Binary
+
+# Given Decimal, Convert to Oct
+
+# Given Decimal, Convert to Hex
+
+# Given Oct, Convert to Decimal
+
+# Given Hex, Convert to Decimal
+
+# Given Oct, Convert to Hex
+
+# Given Hex, Convert to Oct
+
+# Given Unsigned Int Binary String, Convert to Decimal
+
+# Given Binary String, Convert to ASCII
+# Note that MSB is ignored as signs do not impact ASCII characters
+# This stays consistent with the CS 25000 "Computer Architecture" Course Curriculum at Purdue University
+
+# Given 1C, Convert to Decimal
+
+# Given 2C, Convert to Decimal
+
+# Given 2C, Convert from Xbits to Ybits (increasing bits)
+# not sure if decreasing bits would be possible
