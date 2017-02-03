2bf9ef8a19fb0cccf505ccef6f474fd796ac550c
rju <rju@purdue.edu>
2017-02-03 18:33:50 -0500

    Basic request function works

--- newEpisode.py
+++ newEpisode.py
@@ -1,5 +1,22 @@
 # newEpisode.py
 
 # Automatically sends reminders via Mac OS X's Notification Center when a new episode for selected series comes out
-# Current implementation is specific to Kissanime.ru
+# Current implementation is specific to masterani.me
 
+import requests
+# Test to make sure requests is working properly
+# request = requests.get('http://www.google.com')
+
+# Fails, seems to be issues with kissanime.ru's link
+# request = requests.get('http://kissanime.ru/Anime/Youjo-Senki/Episode-005?id=134058')
+
+# Success
+# request = requests.get('http://www.masterani.me')
+request = requests.get('http://www.masterani.me/anime/watch/2386-youjo-senki/2')
+
+if request.status_code < 400:
+	print 'Website Exists'
+	print request.status_code
+else:
+	print 'Website does not exist'
+	print request.status_code
