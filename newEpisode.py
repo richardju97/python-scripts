a4cbc1d9f9c6f51b6099d7869ec04c47ad5c7c15
rju <rju@purdue.edu>
2017-02-03 18:20:19 -0500

    Initial commit of newEpisode.py

---
+++ newEpisode.py
@@ -0,0 +1,5 @@
+# newEpisode.py
+
+# Automatically sends reminders via Mac OS X's Notification Center when a new episode for selected series comes out
+# Current implementation is specific to Kissanime.ru
+
