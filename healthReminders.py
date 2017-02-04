1622381e8c7be0aad86ee65720759fd3c5cd6b49
rju <rju@purdue.edu>
2017-02-04 03:43:51 -0500

    Initial commit for healthReminders.py

---
+++ healthReminders.py
@@ -0,0 +1,6 @@
+# healthReminders.py
+
+# Python Script that reminds you various things to try and improve your health/lifestyle
+# Namely, reminders to drink water as well as take a break from the computer screen
+
+# Take a break for 5 minutes every hour
