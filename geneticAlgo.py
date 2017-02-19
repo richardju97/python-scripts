2688e2567f565da76e4b41ee3b4a6222b1c9b76c
rju <rju@purdue.edu>
2017-02-19 05:58:17 -0600

    Added Uniform Cross Over method

--- geneticAlgo.py
+++ geneticAlgo.py
@@ -1,7 +1,8 @@
 # Given two parent binary strings, computes an offspring binary string based on the selected genetic algorithm
 
-# Single Point Cross Over
+import random
 
+# Single Point Cross Over
 def singleXOver(parent1, parent2, n):
 	if (len(parent1) > n):
 		return parent1[:n] + parent2[n:]
@@ -11,8 +12,16 @@ def singleXOver(parent1, parent2, n):
 def doubleXOver(parent1, parent2, n, m):
 	if (len(parent1) > n and len(parent1) > m):
 		return parent1[:n] + parent2[n:m] + parent1[m:]
-# Uniform Cross Over
 
+# Uniform Cross Over
+def uniformXOver(parent1, parent2):
+	child = ""
+	for i in range(0, len(parent1)):
+		if (bool(random.getrandbits(1))):
+			child += parent1[i]
+		else:
+			child += parent2[i]
+	return child
 
 # Arithmetic Cross Over
 
@@ -23,3 +32,4 @@ parent1 = '11001011'
 parent2 = '11011111'
 # print singleXOver(parent1, parent2, 5)
 # print doubleXOver(parent1, parent2, 2, 7)
+print uniformXOver(parent1, parent2)
