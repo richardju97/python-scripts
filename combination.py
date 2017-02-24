ec73df88791671ec60113d27490284551851febc
rju <rju@purdue.edu>
2017-02-24 00:58:29 -0500

    Modified with OO to be reusable

--- combination.py
+++ combination.py
@@ -1,18 +1,38 @@
 # Calculates Combination (nCr), where 0 <= r <= n
+# NOTE: may be a more efficient method of calculating factorials
+# http://cs.stackexchange.com/questions/14456/factorial-algorithm-more-efficient-than-naive-multiplication
 
-n = 6
-r = 4
+# n = 6
+# r = 4
 
-numerator = 1
-for i in range(r+1, n+1):
-	numerator *= i
+def C(n, r):
+# For efficiency, we factor out r! from n! to minimize the amount of computation time needed (especially for larger combinations)
+	numerator = 1
+	for i in range(r+1, n+1):
+		numerator *= i
 
-diff = n - r
+	diff = n - r
 
-denominator = 1
-for i in range(1, diff+1):
-	denominator *= i
+	denominator = 1
+	for i in range(1, diff+1):
+		denominator *= i
 	
-nCr = numerator / denominator
+	nCr = numerator / denominator
 
-print nCr
+	return nCr;
+
+# numerator = 1
+# for i in range(r+1, n+1):
+# 	numerator *= i
+# 
+# diff = n - r
+# 
+# denominator = 1
+# for i in range(1, diff+1):
+# 	denominator *= i
+# 	
+# nCr = numerator / denominator
+# 
+# print nCr
+
+print C(6, 4)
