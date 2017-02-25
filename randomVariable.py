fe64a576de980992d39d1639c55f4441c0d8b73b
rju <rju@purdue.edu>
2017-02-24 22:18:42 -0600

    Updated most discrete RV with PMF, Expected Value, and Variance functions

--- randomVariable.py
+++ randomVariable.py
@@ -8,22 +8,28 @@ from math import pow, sqrt
 
 # Bernoulli Random Variable
 # X~Bernoulli(p), p = probability of success
-def bernoulli():
-	return 0
+class Bernoulli:
+	def __init__(self, p):
+		self.p = p
+		self.q = 1 - p
 	
-# Expected Value
-# E(x) = p
-def ex_bernoulli():
-	return 0
+# 	Probability mass Function, Px(x) or P(X = x)
+	def pmf(self):
+		return self.p
 	
-# Variance
-# Var(x) = pq
-def var_bernoulli():
-	return 0
+# 	Expected Value
+# 	E(x) = p
+	def expected(self):
+		return self.p
+	
+# 	Variance
+# 	Var(x) = pq
+	def variance(self):
+		return self.p * self.q
 
 # Binomial Random Variable
 # X~Bin(n, p), n = total number of trials, p = probability of success
-class binomial:
+class Binomial:
 	def __init__(self, n, p):
 		self.n = n
 		self.p = p
@@ -38,51 +44,73 @@ class binomial:
 		return probability
 
 # 	Expected Value, E(x) = np
-	def expected():
+	def expected(self):
 		return self.n * self.p
 	
 # 	Var(x) = npq
-	def variance():
+	def variance(self):
 		return self.n * self.p * self.q
 	
 # Geometric Random Variable (success)
-def geo_success():
-	return 0
-
-# Geometric Random Variable (failure)
-def geo_failure():
-	return 0
-
-# E(x) = 1/p	
-def ex_geo():
-	return 0
+class Geometric():
+	def __init__(self, p):
+		self.p = p
+		self.q = 1 - p
 	
-# Var(x) = q/(p^2)
-def var_geo():
-	return 0
+	def geo_success():
+# 	q ^ (x-1) * p
+		return 0
+
+# 	Geometric Random Variable (failure)
+	def geo_failure():
+# 	q ^ x * p
+		return 0
+
+# 	E(x) = 1/p	
+	def expected():
+		return (1/self.p)
+	
+# 	Var(x) = q/(p^2)
+	def variance():
+		return (self.q / pow(self.p, 2))
 
 # Negative Binomial Random Variable
-def NB():
-	return 0
+class NegativeBinomial:
+	def __init__(self, r, p):
+		self.r = r
+		self.p = p
+		self.q = 1 - p
 
-# E(x) = r/p
-def ex_NB():
-	return 0
+	def pmf(x):
+		coefficient = C(x-1, self.r-1)
+		success = pow(self.p, self.r)
+		failure = pow(self.q, x-self.r)
+		return coefficient * success * failure
+
+# 	E(x) = r/p
+	def expected(self):
+		return r / p
 	
-# Var(x) = rq/(p^2)
-def var_NB():
-	return 0
+# 	Var(x) = rq/(p^2)
+	def variance(self):
+		return (r * q / pow(p, 2))
 
 # Poisson Random Variable
 # X~Poisson(lambda), lambda = number of occurrences in a given time frame
-def poisson():
-	return 0
+# Note that we use l in place of lambda as lambda is a reserved word in python
+class Poisson:
+	def __init__(self, l):
+		self.l = l
+
+	def pmf(x):
+# 		e**-l * l**x / x!, need to figure out factorial
+		return 0
 	
-# E(x) = lambda
-def ex_possion():
-	return 0
+# 	E(x) = lambda
+	def expected(self):
+		return self.l
 	
-# Var(x) = lambda
-def var_poission():
-	return 0
+# 	Var(x) = lambda
+	def variance(self):
+		return self.l
 
