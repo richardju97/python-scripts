02598698c1465aaab021667a59d04cb39521f3af
rju <rju@purdue.edu>
2017-02-28 16:36:05 -0500

    Added input system for option selection

--- PracticeExam.py
+++ PracticeExam.py
@@ -9,3 +9,10 @@ if (input == 1):
 
 
 # Function for selecting practice exam vs practice problems
+optionList = "1. Practice Problems\n 2. Practice Exam"
+print optionList
+option = input("Select your practice option: ")
+if (input == 1):
+	print "Practice Problem option selected"
+elif (input == 2):
+	print "Practice Exam option selected"
