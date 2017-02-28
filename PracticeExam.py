ebf7ef7d3621cfb2bb764ed72ba8f310f386a209
rju <rju@purdue.edu>
2017-02-28 16:33:25 -0500

    Added topic selection function for Physics

--- PracticeExam.py
+++ PracticeExam.py
@@ -1,5 +1,11 @@
 # PracticeExam.py
 
 # Function for selecting topic / course
+topicList = "1. Physics\n"
+print topicList
+topic = input("Select your topic of choice: ")
+if (input == 1):
+	print "Physics selected."
+
 
 # Function for selecting practice exam vs practice problems
