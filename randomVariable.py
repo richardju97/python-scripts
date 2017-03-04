5adee562c2e724dc15488663d9fbc4e9ee1b9247
rju <rju@purdue.edu>
2017-03-03 23:10:23 -0500

    Added summation function of Geometric Random Variables

--- randomVariable.py
+++ randomVariable.py
@@ -49,6 +49,29 @@ class Binomial:
 	def variance(self):
 		return self.n * self.p * self.q
 	
+# Negative Binomial Random Variable
+# X~NB(r, p), rth success of multiple independent trials with p = probability of success
+class NegativeBinomial:
+	def __init__(self, r, p):
+		self.r = r
+		self.p = p
+		self.q = 1 - p
+
+# 	Probability Mass Function, Px(x) or P(X = x)
+	def pmf(x):
+		coefficient = C(x-1, self.r-1)
+		success = pow(self.p, self.r)
+		failure = pow(self.q, x-self.r)
+		return coefficient * success * failure
+
+# 	Expected Value, E(x) = r/p
+	def expected(self):
+		return r / p
+	
+# 	Variance, Var(x) = rq/(p^2)
+	def variance(self):
+		return (r * q / pow(p, 2))	
+	
 # Geometric Random Variable
 # X~Geometric(p), p = probability of success
 class Geometric():
@@ -72,28 +95,10 @@ class Geometric():
 	def variance():
 		return (self.q / pow(self.p, 2))
 
-# Negative Binomial Random Variable
-# X~NB(r, p), rth success of multiple independent trials with p = probability of success
-class NegativeBinomial:
-	def __init__(self, r, p):
-		self.r = r
-		self.p = p
-		self.q = 1 - p
-
-# 	Probability Mass Function, Px(x) or P(X = x)
-	def pmf(x):
-		coefficient = C(x-1, self.r-1)
-		success = pow(self.p, self.r)
-		failure = pow(self.q, x-self.r)
-		return coefficient * success * failure
-
-# 	Expected Value, E(x) = r/p
-	def expected(self):
-		return r / p
-	
-# 	Variance, Var(x) = rq/(p^2)
-	def variance(self):
-		return (r * q / pow(p, 2))
+# Returns the summation of r geometric random variables, or a Negative Binomial Random Variable with r=r and probability p
+	def summation(r):
+		sumOfGeometric = NegativeBinomial(r, self.p)
+		return sumOfGeometric
 
 # Poisson Random Variable
 # X~Poisson(lambda), lambda = number of occurrences in a given time frame
