35e183f879d93eab8329dcd1553b4b80f8eba6b3
rju <rju@purdue.edu>
2017-03-05 18:43:34 -0500

    Added comments regarding Poisson Random Variables

--- testRV.py
+++ testRV.py
@@ -1,10 +1,11 @@
 # Test File for randomVariable.py
 # Things to Test: 
-# 	Different Random Variables
+# 	Different Random Variables (Bernoulli, Binomial, Geometric, Negative Binomial, Poisson)
 # 	Expected Value and Variance of each Random Variable
 # 	Summation of Bernoulli (Binomial)
 # 	Summation of Binomial (Binomial) - function doesn't exist yet
 # 	Summation of Geometric (Negative Binomial)
 # 	Summation of Negative Binomial (Negative Binomial) - function doesn't exist yet
+# 	Summation of Poisson (Poisson) - function doesn't exist yet
 
 # Test Bernoulli Random Variables
