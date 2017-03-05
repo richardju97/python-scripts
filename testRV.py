711a671a720522cf925c8b3347a8adfc96287dc7
rju <rju@purdue.edu>
2017-03-05 18:41:50 -0500

    Added comments for which functions need testing

--- testRV.py
+++ testRV.py
@@ -1,3 +1,10 @@
 # Test File for randomVariable.py
+# Things to Test: 
+# 	Different Random Variables
+# 	Expected Value and Variance of each Random Variable
+# 	Summation of Bernoulli (Binomial)
+# 	Summation of Binomial (Binomial) - function doesn't exist yet
+# 	Summation of Geometric (Negative Binomial)
+# 	Summation of Negative Binomial (Negative Binomial) - function doesn't exist yet
 
 # Test Bernoulli Random Variables
