f94ee2d3cbb44a7fb931ac0a6ee8f229f4998dd6
rju <rju@purdue.edu>
2017-03-06 18:48:31 -0500

    Initial commit for GradesCalculator.py

---
+++ GradesCalculator.py
@@ -0,0 +1,8 @@
+# Grades Calculator
+
+# Category Objects for Weighting, Item Objects for Assignments, Quizzes, Exams, etc.
+# Course Objects for each class (course)
+
+# Item Object should have date due? Points earned, Points Possible, and calc percentage function
+
+# Some sort of graphing function to see your grades over time (and gpa over time)
