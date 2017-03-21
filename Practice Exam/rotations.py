1adde074b12f0e06546770116ba44a2f7d0e1bb9
rju <rju@purdue.edu>
2017-03-20 23:39:04 -0400

    Finished ConstantRotatatingObject class

--- Practice Exam/rotations.py
+++ Practice Exam/rotations.py
@@ -1,9 +1,24 @@
 # rotations.py
 
+import math
+
 class ConstantRotatingObject:
 	def __init__(self):
 		self.radius = None
+		self.rotationalSpeed = None
+		self.translationalSpeed = None
 		
 	def setRadius(self, r):
 		self.radius = r
 		
+	def setRotationalSpeed(self, o):
+		self.rotationalSpeed = o
+		
+	def setTranslationalSpeed(self, v):
+		self.translationalSpeed = v
+		
+	def calcRotationalSpeed(self, t):
+		return 2.0 * PI / t
+		
+	def calcTranslationalSpeed(self, t):
+		return 2.0 * PI * self.radius / t
