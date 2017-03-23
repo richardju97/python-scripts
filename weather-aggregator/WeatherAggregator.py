92307fdf4a1d2679a6f1cdd1d1128ec7a3c4c176
rju <rju@purdue.edu>
2017-03-23 10:40:23 -0400

    Initial commit of WeatherAggregator.py

---
+++ weather-aggregator/WeatherAggregator.py
@@ -0,0 +1,4 @@
+# weather-aggregator.py
+
+# Main script that runs in the background upon launch every day until specified end date
+# Calls weather.py at specified intervals to obtain weather data 
