31dc6ce857d42d1f599fa4e0419128113971b4e3
rju <rju@purdue.edu>
2017-04-04 16:53:22 -0400

    Initial commit, has average function

---
+++ lab.py
@@ -0,0 +1,9 @@
+# lab.py
+# Various calculations relevant to generic physics laboratory assignments
+
+# Average
+array = input("Insert array of values here: ")
+sum = 0.0
+for i in array:
+	sum += i
+print sum/len(array)	
