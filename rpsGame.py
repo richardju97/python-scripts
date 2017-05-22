7eb7a8f2a99663f3a861afbfd08d5d994e9364db
rju <rju@purdue.edu>
2017-05-22 16:34:21 -0700

    Modified into a function so we can repeatedly call and apply statistical analysis

--- rpsGame.py
+++ rpsGame.py
@@ -3,59 +3,66 @@
 import sys
 from random import randint
 
-n = int(sys.argv[1])
+#n = int(sys.argv[1])
 matches = 0
 
-lineA = []
-lineB = []
+#lineA = []
+#lineB = []
 
-for i in range(0, n):
-	lineA.append("personA" + str(i))
-	lineB.append("personB" + str(i))
 
 #0 = Tie, 1 = A wins, 2 = B wins
 def compete(idxA, idxB):
 	global matches 
 	matches += 1
 	return randint(0, 299) % 3	
-
-aSize = len(lineA)
-bSize = len(lineB)
-while (aSize != 0 and bSize != 0):
-	i = 0
-	j = 0
-	temp = compete(i, j)
-	while (temp == 0):
-		print "Tie!!"
-		if (i < aSize-1):
-			i += 1
-		if (j < bSize-1):
-			j += 1
-		temp = compete(i, j)
-		
-	if (temp == 1):
-		print "Person A won!"
-		if (j == 0):
-			lineA.append(lineB[0])
-			del lineB[0]
-		else:
-			lineA.extend(lineB[0:j])
-			del lineB[0:j]
-		print "Line A: " + str(lineA)
-		print "Line B: " + str(lineB)
-	elif (temp == 2):
-		print "Person B won!"
-		if (i == 0):
-			lineB.append(lineA[0])
-			del lineA[0]
-		else:
-			lineB.extend(lineA[0:i])		
-			del lineA[0:i]
-		print "Line A: " + str(lineA)
-		print "Line B: " + str(lineB)
+def playGame(n):
+	
+	lineA = []
+	lineB = []
+	
+	for i in range(0, n):
+		lineA.append("personA" + str(i))
+		lineB.append("personB" + str(i))
+	
 	aSize = len(lineA)
-	bSize = len(lineB)	
+	bSize = len(lineB)
+	while (aSize != 0 and bSize != 0):
+		i = 0
+		j = 0
+		temp = compete(i, j)
+		while (temp == 0):
+			print "Tie!!"
+			if (i < aSize-1):
+				i += 1
+			if (j < bSize-1):
+				j += 1
+			temp = compete(i, j)
+		
+		if (temp == 1):
+			print "Person A won!"
+			if (j == 0):
+				lineA.append(lineB[0])
+				del lineB[0]
+			else:
+				lineA.extend(lineB[0:j])
+				del lineB[0:j]
+			print "Line A: " + str(lineA)
+			print "Line B: " + str(lineB)
+		elif (temp == 2):
+			print "Person B won!"
+			if (i == 0):
+				lineB.append(lineA[0])
+				del lineA[0]
+			else:
+				lineB.extend(lineA[0:i])		
+				del lineA[0:i]
+			print "Line A: " + str(lineA)
+			print "Line B: " + str(lineB)
+		aSize = len(lineA)
+		bSize = len(lineB)	
 
-print "--------------------"
-print "Game Finished!"
-print "The Game took " + str(matches) + " matches!"
+	print "--------------------"
+	print "Game Finished!"
+	print "The Game took " + str(matches) + " matches!"
+
+playGame(500)
