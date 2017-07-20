7cb8dff4482881c63c0f294f94470881df3d31d1
rju <rju@purdue.edu>
2017-07-20 16:26:03 +0800

    INIT

---
+++ message.py
@@ -0,0 +1,4 @@
+# message.py
+# Fills in certain blanks based off an array (presumably names) into a given message template# message.py
+# Fills in certain blanks based off an array (presumably names) into a given message template# message.py
+# Fills in certain blanks based off an array (presumably names) into a given message template
