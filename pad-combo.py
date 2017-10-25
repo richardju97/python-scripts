4d1bb706db647f7e17484345f957d6e284d11fb9
rju <rju@purdue.edu>
2017-10-25 05:29:10 -0400

    added some stuff

--- pad-combo.py
+++ pad-combo.py
@@ -1,2 +1,11 @@
 # pad-combo.py
 
+class Board:
+    def __init__(r, c):
+        self.rows = r
+        self.columns = c
+        self.b = []
+        self.createBoard()
+    def createBoard():
+        for i in range(0, self.rows*self.columns):
+            b.append(0)
