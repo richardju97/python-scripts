1f09a3e8575bfd867c26d4582d7d2759bb9779dd
rju <rju@purdue.edu>
2018-01-05 17:30:37 +0800

    INIT

---
+++ networkAddress.py
@@ -0,0 +1,15 @@
+# networkAddress.py
+
+# Some basic converters related to networks
+
+import sys
+
+# 32bit Binary String -> IPv4 Address
+str = sys.argv[1]
+#print(str[1])
+
+# IPv4 Address -> 32bit Binary String
+
+# 48bit Binary String -> Ethernet/Hardware/MAC Address
+
+# Ethernet/Hardware/MAC Address -> 48bit Binary String
