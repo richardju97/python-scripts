d6298d5517518434118958afbcdf8c8b0bdb2432
rju <rju@purdue.edu>
2018-01-16 16:28:04 +0800

    successfully calls ping with subprocess

--- websitestatus.py
+++ websitestatus.py
@@ -2,4 +2,7 @@
 # python script that periodically pings a website url to check if it's still live
 # notifications are sent in the event that the specified site is down
 
+import subprocess
 
+s = subprocess.check_output(['ping', '-o', 'data.itasa.org'])
+print(s)
