f42c8b665228e0dc174fa8b8ba42fe9d4b03fbd3
rju <rju@purdue.edu>
2018-04-12 16:01:28 +0900

    Added factorial function

---
+++ powersum.py
@@ -0,0 +1,15 @@
+# powersum.py
+# script for computing powersums based on Class 1 of Mathematics B at Tohoku University (special JYPE course)
+
+# The kth power sum is defined as
+# 1^k + 2^k + ... + n^k
+
+from math import factorial
+
+# Combination
+# Function for computing Combinations nCr
+
+def ncr(n, r):
+    print(factorial(n))
+
+ncr(4, 0)
