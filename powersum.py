4d528ac71aa5c61b9fb3faac18c8e65e7ff91f42
rju <rju@purdue.edu>
2018-04-25 20:44:14 +0900

    Clean/Added Comments

--- powersum.py
+++ powersum.py
@@ -9,14 +9,12 @@ from math import pow
 
 # Combination
 # Function for computing Combinations nCr
-
 def ncr(n, r):
     combination = factorial(n) / (factorial(n - r) * factorial(r))
     return combination
 
 # Bernoulli Number
 # Function for computing Bernoulli Numbers
-
 def bernoullinum(n):
     if (n == 0):
         return 1
@@ -26,6 +24,7 @@ def bernoullinum(n):
             sum -= ncr(n+1, i) * bernoullinum(i)
         return 1/(n + 1) * sum
 
+# Function for computing b, used for calculating the power sum
 def calcb(n):
     if (n == 1):
         return 1
@@ -34,11 +33,12 @@ def calcb(n):
     else:
         return bernoullinum(n-1)
 
+# Function for calculating the power sum with inputs k and n
 def powersum(k, n):
     sum = 0
     for i in range(0, k+1):
         sum += ncr(k+1, i) * calcb(i + 1) * (pow(n, k+1-i))
-
     return 1/(k+1) * sum
 
+# Function Calls for Testing
 print(powersum(4, 2))
